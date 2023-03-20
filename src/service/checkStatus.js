170320444099e368b320f87e375081061e800759
Martin <martindrus@outlook.de>
2023-03-20 10:35:39 +0100

    fixes

--- src/service/checkStatus.js
+++ src/service/checkStatus.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 export async function userStatus(){
     try {
-        let response = await axios.get('http://localhost:8080/auth/status',{
+        let response = await axios.get('https://event-calender-backend-production.up.railway.app/auth/status',{
             withCredentials: true
         })
         if(!response.data) response.data = {success: false, message: 'Token missing!'}
         return response.data;
     } catch (error) {
+        console.log("🚀 ~ file: checkStatus.js:11 ~ userStatus ~ error:", error)
         if(!error.response.data) error.response.data = {success: false, message: 'Token missing!'}
         return error.response.data
     }
